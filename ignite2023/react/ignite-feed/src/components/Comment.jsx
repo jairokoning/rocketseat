e5c21e0e81fe68af6c557dbb615f2ae2aa8bc522
Jairo Koning <jairo.koning@gmail.com>
2023-07-26 14:57:37 -0300

    feat: like comment

--- ignite2023/react/ignite-feed/src/components/Comment.jsx
+++ ignite2023/react/ignite-feed/src/components/Comment.jsx
@@ -1,9 +1,16 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react';
 export function Comment({ comment, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+  
   function handleDeleteComment() {
     onDeleteComment(comment);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -23,9 +30,9 @@ export function Comment({ comment, onDeleteComment }) {
           <p>{comment}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
